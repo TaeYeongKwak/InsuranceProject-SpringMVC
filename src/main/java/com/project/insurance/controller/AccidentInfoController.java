40af7b11e7790b910bff6f5e98a1a749691014b3
TaeYeongKwak <abgudv6403@naver.com>
2021-11-20 01:59:36 +0900

    controller 일부 구현

--- src/main/java/com/project/insurance/controller/AccidentInfoController.java
+++ src/main/java/com/project/insurance/controller/AccidentInfoController.java
@@ -1,9 +1,32 @@
 package com.project.insurance.controller;
 
+import java.sql.SQLException;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.project.insurance.service.ContractService;
 
 @Controller
 public class AccidentInfoController {
 	//사고 기록을 보여주는 화면의 컨트롤러
+	private final ContractService contractService;
+	
+	public AccidentInfoController(ContractService contractService) {
+		this.contractService = contractService;
+	}
+	
+	@RequestMapping(value="accident/{accident_num}", method = RequestMethod.GET)
+	public String accidentInfo(Model model, @PathVariable int accidentNum) {
+		try {
+			model.addAttribute("accident", contractService.searchByAccidentNum(accidentNum));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "accidentInfo";
+	}
 	
 }
--- src/main/java/com/project/insurance/controller/AccidentListController.java
+++ src/main/java/com/project/insurance/controller/AccidentListController.java
@@ -1,10 +1,34 @@
 package com.project.insurance.controller;
 
+import java.sql.SQLException;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.project.insurance.service.ContractService;
+import com.project.insurance.type.InsuranceProductType;
 
 @Controller
 public class AccidentListController {
 	//사고 리스트를 보여주는 화면의 컨트롤러
+	private final ContractService contractService;
 	
+	public AccidentListController(ContractService contractService) {
+		this.contractService = contractService;
+	}
+	
+	@RequestMapping(value="accident/list", method = RequestMethod.GET)
+	public String showList(Model model, @RequestParam("TYPE")String type) {
+		try {
+			model.addAttribute("accidentList",
+					contractService.searchAccidentListByProductType(InsuranceProductType.valueOf(type)));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "accidentList";
+	}
 	
 }
