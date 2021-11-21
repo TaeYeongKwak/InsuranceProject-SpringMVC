db3cfa0b34cbafea4f0ba4ac5c2e6498d74f8c4e
TaeYeongKwak <abgudv6403@naver.com>
2021-11-21 18:21:46 +0900

    dao, mapper 오류 확인후 수정

--- src/main/java/com/project/insurance/dao/AccidentDaoImpl.java
+++ src/main/java/com/project/insurance/dao/AccidentDaoImpl.java
@@ -1,13 +1,20 @@
 package com.project.insurance.dao;
 
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 import com.project.insurance.model.Accident;
+import com.project.insurance.model.Client;
+import com.project.insurance.model.insurance.InsuranceProduct;
 
 @Repository
 public class AccidentDaoImpl implements AccidentDao{
@@ -20,28 +27,56 @@ public class AccidentDaoImpl implements AccidentDao{
 
 	@Override
 	public boolean add(Accident accident) throws SQLException{
-		int value = sqlSession.insert("accident_insert", accident);
-		System.out.println(value);
-		return true;
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("clientId", accident.getClient().getId());
+		map.put("insuranceProductName", accident.getInsuranceProduct().getProductName());
+		map.put("accidentDetail", accident.getAccidentDetail());
+		DateFormat dateformat = new SimpleDateFormat("yyyy-mm-dd");
+		map.put("receptionDate", dateformat.format(accident.getReceptionDate()));
+		return sqlSession.insert("accident_insert", map) == 1? true : false;
 	}
 
 	@Override
 	public boolean delete(Accident accident) throws SQLException{
-		int value = sqlSession.delete("accident_deleteOne", accident.getAccidentNum());
-		System.out.println(value);
-		return true;
+		return sqlSession.delete("accident_deleteOne", accident.getAccidentNum()) == 1? true : false;
 	}
 
 	@Override
 	public Accident search(int accidentNum) throws SQLException{
-		Accident accident = sqlSession.selectOne("accident_selectOne", accidentNum);
-		return accident;
+		Map<String, String> map = sqlSession.selectOne("accident_selectOne", accidentNum);
+		return toAccident(map);
 	}
 
 	@Override
 	public ArrayList<Accident> findAll() throws SQLException{
-		List<Accident> accidentList = sqlSession.selectList("accident_selectAll");
-		return new ArrayList<Accident>(accidentList);
+		List<Map<String, String>> accidentMapList = sqlSession.selectList("accident_selectAll");
+		ArrayList<Accident> list = new ArrayList<Accident>();
+		for(Map<String, String> map : accidentMapList)
+			list.add(toAccident(map));
+		return list;
+	}
+	
+	private Accident toAccident(Map<String, String> map){
+		if(map != null) {
+			Accident accident = new Accident();
+			try {
+				accident.setAccidentNum(Integer.parseInt(String.valueOf(map.get("accident_num"))));
+				Client client = new Client();
+				client.setId(map.get("client_Id"));
+				accident.setClient(client);
+				InsuranceProduct insuranceProduct = new InsuranceProduct();
+				insuranceProduct.setProductName(String.valueOf(map.get("insurance_product_name")));
+				accident.setInsuranceProduct(insuranceProduct);
+				accident.setAccidentDetail(String.valueOf(map.get("accident_detail")));
+				SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+				accident.setReceptionDate(dateFormat.parse(String.valueOf(map.get("reception_date"))));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			
+			return accident;
+		}else
+			return null;
 	}
 
 }
--- src/main/java/com/project/insurance/dao/ContractDaoImpl.java
+++ src/main/java/com/project/insurance/dao/ContractDaoImpl.java
@@ -2,9 +2,12 @@ package com.project.insurance.dao;
 
 import java.sql.Date;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
@@ -26,41 +29,39 @@ private final SqlSession sqlSession;
 	@Override
 	public boolean add(Contract contract) throws SQLException{
 		HashMap<String, Object> map = new HashMap<String, Object>();
-		map.put("insuranceContractDate", contract.getInsuranceContractDate().getTime());
-		map.put("insuranceExpiryDate", contract.getInsuranceExpiryDate().getTime());
-		map.put("managerId", contract.getSalesPerson().getName());
+		map.put("insuranceContractDate",contract.getInsuranceContractDate());
+		map.put("insuranceExpiryDate", contract.getInsuranceExpiryDate());
+		map.put("managerId", contract.getSalesPerson().getId());
 		map.put("approval", contract.isApproval()? 1:0);
 		map.put("months", 0);
 		map.put("clientID", contract.getClient().getId());
 		map.put("insuranceProductName", contract.getInsuranceProduct().getProductName());
-		
-		int value = sqlSession.insert("contract_insert", map);
-		System.out.println(value);
-		return true;
+		return sqlSession.insert("contract_insert", map) == 1? true : false;
 	}
 
 	@Override
 	public boolean delete(Contract contract) throws SQLException{
-		int value = sqlSession.delete("contract_delete", contract);
-		System.out.println(value);
-		return true;
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("clientID", contract.getClient().getId());
+		map.put("insuranceProductName", contract.getInsuranceProduct().getProductName());
+		return sqlSession.delete("contract_deleteOne", map) > 0? true : false;
 		
 	}
 
 	@Override
 	public Contract search(String clientID, String productName) throws SQLException{
 		HashMap<String, String> map = new HashMap<String, String>();
-		map.put("clientId", clientID);
+		map.put("clientID", clientID);
 		map.put("insuranceProductName", productName);
 		
-		HashMap<String, Object> contractMap = sqlSession.selectOne("contract_selectOne", map);
+		HashMap<String, String> contractMap = sqlSession.selectOne("contract_selectOne", map);
 		return this.toContract(contractMap);
 		
 	}
 
 	@Override
 	public ArrayList<Contract> searchByClient(String clientID) throws SQLException{
-		List<HashMap<String, Object>> contractList = sqlSession.selectList("contract_selectClientId", clientID);
+		List<HashMap<String, String>> contractList = sqlSession.selectList("contract_selectClientId", clientID);
 		ArrayList<Contract> contractArray = new ArrayList<Contract>();
 		for(int i = 0; i < contractList.size(); i++)
 			contractArray.add(this.toContract(contractList.get(i)));
@@ -69,7 +70,7 @@ private final SqlSession sqlSession;
 
 	@Override
 	public ArrayList<Contract> searchBySalesPerson(String salesPerson) throws SQLException{
-		List<HashMap<String, Object>> contractList = sqlSession.selectList("contract_selectManagerId", salesPerson);
+		List<HashMap<String, String>> contractList = sqlSession.selectList("contract_selectManagerId", salesPerson);
 		ArrayList<Contract> contractArray = new ArrayList<Contract>();
 		for(int i = 0; i < contractList.size(); i++)
 			contractArray.add(this.toContract(contractList.get(i)));
@@ -87,38 +88,39 @@ private final SqlSession sqlSession;
 		map.put("clientID", contract.getClient().getId());
 		map.put("insuranceProductName", contract.getInsuranceProduct().getProductName());
 		
-		
-		int value = sqlSession.selectOne("contract_update", map);
-		System.out.println(value);
-		return true;
+		return sqlSession.update("contract_update", map) > 0 ? true : false;
 		
 	}
 
 	@Override
 	public ArrayList<Contract> findAll() throws SQLException{
-		
-		List<HashMap<String, Object>> contractList = sqlSession.selectList("contract_selectAll");
+		List<HashMap<String, String>> contractList = sqlSession.selectList("contract_selectAll");
 		ArrayList<Contract> contractArray = new ArrayList<Contract>();
 		for(int i = 0; i < contractList.size(); i++)
 			contractArray.add(this.toContract(contractList.get(i)));
 		return contractArray;
 	}
 	
-	private Contract toContract(HashMap<String, Object> contractMap) {
+	private Contract toContract(HashMap<String, String> contractMap) {
 		Contract contract = new Contract();
 		Client client = new Client();
-		client.setId(contractMap.get("client_id").toString());
+		client.setId(String.valueOf(contractMap.get("client_id")));
 		contract.setClient(client);
 		InsuranceProduct insuranceProduct = new InsuranceProduct();
-		insuranceProduct.setProductName(contractMap.get("insurance_product_name").toString());
+		insuranceProduct.setProductName(String.valueOf(contractMap.get("insurance_product_name")));
 		contract.setInsuranceProduct(insuranceProduct);
-		contract.setInsuranceContractDate((Date)contractMap.get("insurance_contract_date"));
-		contract.setInsuranceExpiryDate((Date)contractMap.get("insurance_expiry_date"));
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+		try {
+			contract.setInsuranceContractDate(dateFormat.parse(String.valueOf(contractMap.get("insurance_contract_date"))));
+			contract.setInsuranceExpiryDate(dateFormat.parse(String.valueOf(contractMap.get("insurance_expiry_date"))));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 		Manager manager = new Manager();
-		manager.setId(contractMap.get("manager_id").toString());
+		manager.setId(String.valueOf(contractMap.get("manager_id")));
 		contract.setSalesPerson(manager);
-		contract.setApproval((int)contractMap.get("approval") == 1? true : false);
-		contract.setMonth(this.monthBitMasking((int)contractMap.get("months")));
+		contract.setApproval(Integer.parseInt(String.valueOf(contractMap.get("approval"))) == 1? true : false);
+		contract.setMonth(this.monthBitMasking(Integer.parseInt(String.valueOf(contractMap.get("months")))));
 		return contract;
 	}
 	
