7e9100772d6288da26989c280c5e4017fc2a9de8
TaeYeongKwak <abgudv6403@naver.com>
2021-11-23 13:59:42 +0900

    merge fail clear

--- src/main/java/com/project/insurance/HomeController.java
+++ src/main/java/com/project/insurance/HomeController.java
@@ -1,12 +1,6 @@
 package com.project.insurance;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.project.insurance.model.Client;
-import com.project.insurance.model.manager.Manager;
 
 /**
  * Handles requests for the application home page.
