6293282fb866b99c1acc27d541b56141efd0b9dd
TaeYeongKwak <abgudv6403@naver.com>
2021-11-23 13:47:14 +0900

    merge fail clear

--- src/main/java/com/project/insurance/HomeController.java
+++ src/main/java/com/project/insurance/HomeController.java
@@ -1,33 +1,17 @@
 package com.project.insurance;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.project.insurance.model.Client;
+import com.project.insurance.model.manager.Manager;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
-		return "login";
-	}
-	
-//	@RequestMapping(value = "test1", method = RequestMethod.POST)
-//	public String test1(Client client, Model model) {
-//		model.addAttribute("client", client);
-//		return "home";
-//	}
-//	
-//	@RequestMapping(value = "test2", method = RequestMethod.POST)
-//	public String test2(Manager manager, Model model) {
-//		model.addAttribute("manager", manager);
-//		return "home";
-//	}
 	
 }
