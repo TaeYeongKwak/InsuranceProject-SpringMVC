2fbb4964c7baa2b8c38146aa07a27a7f65b0dfcc
ys03055 <33304927+ys03055@users.noreply.github.com>
2021-11-28 01:39:20 +0900

    MedicalHistoryController 수정

--- src/main/java/com/project/insurance/controller/MedicalHistoryController.java
+++ src/main/java/com/project/insurance/controller/MedicalHistoryController.java
@@ -1,8 +1,41 @@
 package com.project.insurance.controller;
 
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.project.insurance.exception.ClientNotFoundException;
+import com.project.insurance.model.Client;
+import com.project.insurance.service.ClientService;
 
 @Controller
 public class MedicalHistoryController {
-	// 의료 기록을 추가하는 화면의 컨트롤러
+	
+	
+	@Autowired
+	private ClientService clientService;
+
+	
+	 @RequestMapping(value="medicalHistory}", method=RequestMethod.GET)
+	 public Object medicalHistory(Client client, Model model, HttpServletRequest request) throws SQLException  {
+		 HttpSession session = request.getSession();
+			Client Cresult = clientService.login(client.getId(), client.getPassword());
+			session.setAttribute("id", Cresult);
+			
+			if (Cresult == null) 
+				throw new ClientNotFoundException();
+			model.addAttribute("client", client);
+			
+				
+			return session.getAttribute("id");
+			
+			
+	    }
 }
