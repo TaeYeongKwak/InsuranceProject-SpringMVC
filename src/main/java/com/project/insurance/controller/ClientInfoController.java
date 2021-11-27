7b1f680109de678a34cf6243d5467f0a8eb47216
ys03055 <33304927+ys03055@users.noreply.github.com>
2021-11-28 00:12:47 +0900

    ClientInfoController 추가

--- src/main/java/com/project/insurance/controller/ClientInfoController.java
+++ src/main/java/com/project/insurance/controller/ClientInfoController.java
@@ -1,8 +1,39 @@
 package com.project.insurance.controller;
 
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.project.insurance.model.Client;
+import com.project.insurance.service.ClientService;
 
 @Controller
 public class ClientInfoController {
-	// 회원의 정보를 확인하는 화면의 컨트롤러, 회원 탈퇴, 정보 수정등이 가능
+	@Autowired
+	private ClientService clientService;
+	
+	 @RequestMapping(value="client/Info", method=RequestMethod.POST)
+	    public Object clientInfo(Client client, Model model, HttpServletRequest request) throws SQLException {
+		 	HttpSession session = request.getSession();
+			Client Cresult = clientService.login(client.getId(), client.getPassword());
+			session.setAttribute("id", Cresult);
+			
+			if (Cresult != null) {
+				return "home";
+			} 
+			
+			else {
+				
+				return session.getAttribute("id");
+			}
+
+		
+	    }
 }
