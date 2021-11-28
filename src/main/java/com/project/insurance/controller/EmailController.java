16076bc7ffb97bddd2f454708416a825a7c17e04
Lee Sueng Jun <zosksldhs@gmail.com>
2021-11-29 00:19:16 +0900

    email 기능 구현

--- src/main/java/com/project/insurance/controller/EmailController.java
+++ src/main/java/com/project/insurance/controller/EmailController.java
@@ -1,8 +1,40 @@
 package com.project.insurance.controller;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.project.insurance.model.Email;
+import com.project.insurance.service.EmailService;
 
 @Controller
 public class EmailController {
-	// 이메일을 보내는 화면의 컨트롤러
+	
+	@Inject
+	EmailService emailservice;
+	
+	   @RequestMapping(value = "email", method = RequestMethod.GET)
+	   public String email() {
+	      return "email";
+	   }
+
+	
+	@RequestMapping(value = "email/send", method = RequestMethod.GET)
+	public String send(@ModelAttribute Email dto, Model model) {
+		try {
+			System.out.println("dd" + dto.toString());
+			emailservice.sendMail(dto);
+			model.addAttribute("message", "이메일 발송이 완료되었습니다.");
+			return "login";
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("message", "이메일 발송이 실패했습니다.");
+		}
+		return "email";
+	}
 }
---
+++ src/main/java/com/project/insurance/service/EmailService.java
@@ -0,0 +1,8 @@
+package com.project.insurance.service;
+
+import com.project.insurance.model.Email;
+
+public interface EmailService {
+
+	public void sendMail(Email dto);
+}
