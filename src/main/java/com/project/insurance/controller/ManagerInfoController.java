3b34c17238b4dc6b1dd1ba3ae633217296997920
Lee Sueng Jun <zosksldhs@gmail.com>
2021-11-30 00:08:14 +0900

    managerinfo 구현

--- src/main/java/com/project/insurance/controller/ManagerInfoController.java
+++ src/main/java/com/project/insurance/controller/ManagerInfoController.java
@@ -1,8 +1,34 @@
 package com.project.insurance.controller;
 
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.project.insurance.model.manager.Manager;
 
 @Controller
 public class ManagerInfoController {
-	// 관리자의 정보를 보여주는 컨트롤러, 관리자 탈퇴, 정보 수정등이 가능
+	@RequestMapping(value = "manager/Info", method = RequestMethod.GET)
+	public String maanagerInfo(Model model, HttpServletRequest request) throws SQLException {
+		HttpSession session = request.getSession();
+		Manager manager = (Manager) session.getAttribute("id");
+		model.addAttribute("managerInfo", manager);
+
+		return "managerInfo";
+	}
+
+	@RequestMapping(value = "manager/InfoModify", method = RequestMethod.GET)
+	public String clientModifyInfo(Model model, HttpServletRequest request) throws SQLException {
+		HttpSession session = request.getSession();
+		Manager manager = (Manager) session.getAttribute("id");
+		model.addAttribute("managerInfo", manager);
+
+		return "managerModifyInfo";
+	}
 }
