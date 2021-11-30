095704bb3fd6226013a9a8857bb873739d0a2c5a
ys03055 <33304927+ys03055@users.noreply.github.com>
2021-11-30 19:28:43 +0900

    합본

---
+++ src/main/java/com/project/insurance/model/Work.java
@@ -0,0 +1,18 @@
+package com.project.insurance.model;
+
+public class Work {
+	private String name;
+	private String url;
+	
+	public Work(String name, String url) {
+		this.name = name;
+		this.url = url;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String getUrl() {
+		return url;
+	}
+}
